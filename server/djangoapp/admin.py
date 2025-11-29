4f9b59b025ade77686130d26c7ad7bd075c5366c
Mark Jankowski <lathon2018@gmail.com>
2025-11-28 23:39:29 -0500

    Car models and Car makes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,10 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
 
@@ -11,3 +13,4 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+https://lathon-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/admin/login/?next=/admin/
