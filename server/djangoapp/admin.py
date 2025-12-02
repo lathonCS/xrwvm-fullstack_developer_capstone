db9c11b9f082a38225ea83f68f1c56e918fece8f
Mark Jankowski <lathon2018@gmail.com>
2025-12-02 14:38:16 -0500

    REmoved accidently addition of a url

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -13,4 +13,3 @@ admin.site.register(CarModel)
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
-https://lathon-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/admin/login/?next=/admin/
